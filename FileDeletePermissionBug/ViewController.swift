5f4d1479e29d1d6a63ed709338eb3d2653963f7b
Giovanni Murru <giovanni.murru@gmail.com>
2024-12-11 10:38:54 +0100

    Add some code to log file permissions.

--- FileDeletePermissionBug/ViewController.swift
+++ FileDeletePermissionBug/ViewController.swift
@@ -93,12 +93,52 @@ class ViewController: UIViewController {
         // Store the delete button and latest error message for later use
         self.deleteFileButton = deleteFileButton
         self.latestErrorMessage = latestErrorMessage
+        
+        if let testfileURL = Bundle.main.url(forResource: "testfile", withExtension: "txt") {
+            print("\n")
+            print("INFO: FILE ATTRIBUTES FOR BUNDLED TEST FILE AT \(testfileURL.path)")
+            printFileAttributes(path: testfileURL.path)
+            let temporaryItemURL = FileManager.default.temporaryDirectory.appendingPathComponent(testfileURL.lastPathComponent)
+            try? FileManager.default.removeItem(at: temporaryItemURL)
+            try? FileManager.default.copyItem(atPath: testfileURL.path, toPath: temporaryItemURL.path)
+            print("\n")
+            print("INFO: FILE ATTRIBUTES FOR SANDBOXED TEST FILE AT \(testfileURL.path)")
+            printFileAttributes(path: temporaryItemURL.path)
+        }
+    }
+    
+    func printFileAttributes(path: String) {
+        
+        if let fileAttributes = try? FileManager.default.attributesOfItem(atPath: path) {
+            if let owner = fileAttributes[.ownerAccountID] {
+                print("INFO: Owner: \(owner)")
+            }
+            if let group = fileAttributes[.groupOwnerAccountID] {
+                print("INFO: Group: \(group)")
+            }
+            if let permissions = fileAttributes[.posixPermissions] as? NSNumber {
+                let octalPermissions = String(permissions.intValue, radix: 8)
+                print("INFO: POSIX Permissions: \(octalPermissions)")
+            }
+        } else {
+            print("WARNING: could not retrieve file attributes")
+        }
+        print("\n")
     }
 
-
+    
+    func changeFilepermission(to permissions: Int, at path: String) throws {
+        let fileManager = FileManager.default
+        
+        // Define the new attributes for the file
+        let newAttributes: [FileAttributeKey: Any] = [
+            .posixPermissions: NSNumber(value: permissions)
+        ]
+        try fileManager.setAttributes(newAttributes, ofItemAtPath: path)
+    }
 
     @objc func openFile(_ sender: UIButton) {
-        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.movie, .text, .pdf, .image])
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.movie, .text, .pdf, .image], asCopy: false)
         documentPicker.delegate = self
         documentPicker.allowsMultipleSelection = false
         documentPicker.shouldShowFileExtensions = true
@@ -110,12 +150,29 @@ class ViewController: UIViewController {
         guard let currentItemURL = currentItemURL else {
             return
         }
-        
+        print("\n")
+        print("INFO: FILE ATTRIBUTES FOR SANDBOXED DELETE FILE AT \(currentItemURL.path)")
+        printFileAttributes(path: currentItemURL.path)
         do {
             try FileManager.default.removeItem(at: currentItemURL)
             self.currentItemURL = nil
         } catch {
-            errorDeletingFile(error: error as NSError, temporaryItemURL: currentItemURL)
+            print("WARNING: Couldn't delete file at \(currentItemURL.path).")
+            print("WARNING: Trying to change permissions to 644 even if this shouldn't be necessary because I am the owner of the file...")
+            do {
+                // Set the new attributes
+                try changeFilepermission(to: 0o644, at: currentItemURL.path)
+                print("INFO: File permissions successfully updated to 644.")
+                do {
+                    try FileManager.default.removeItem(at: currentItemURL)
+                    self.currentItemURL = nil
+                } catch {
+                    errorDeletingFile(error: error as NSError, temporaryItemURL: currentItemURL)
+                }
+            } catch {
+                print("ERROR: Failed to update file permissions: \(error.localizedDescription)")
+                errorDeletingFile(error: error as NSError, temporaryItemURL: currentItemURL)
+            }
             return
         }
         
@@ -137,11 +194,22 @@ extension ViewController: UIDocumentPickerDelegate {
         
         print("INFO: documentPicker did pick source item at url \(sourceItemURL)")
         
+        let securityScoped = sourceItemURL.startAccessingSecurityScopedResource()
+        print("\n")
+        print("INFO: FILE ATTRIBUTES FOR PICKED DOCUMENT AT \(sourceItemURL.path)")
+        printFileAttributes(path: sourceItemURL.path)
+        if securityScoped {
+            sourceItemURL.stopAccessingSecurityScopedResource()
+        }
+        
         let temporaryItemURL = FileManager.default.temporaryDirectory.appendingPathComponent(sourceItemURL.lastPathComponent)
         
         if FileManager.default.fileExists(atPath: temporaryItemURL.path) {
             print("WARNING: Temporary item already exists.")
             currentItemURL = temporaryItemURL
+            print("\n")
+            print("INFO: FILE ATTRIBUTES FOR SANDBOXED PICKED DOCUMENT AT \(temporaryItemURL.path)")
+            printFileAttributes(path: temporaryItemURL.path)
             return
         }
         
@@ -150,6 +218,9 @@ extension ViewController: UIDocumentPickerDelegate {
             let securityScoped = sourceItemURL.startAccessingSecurityScopedResource()
             try FileManager.default.copyItem(at: sourceItemURL, to: temporaryItemURL)
             currentItemURL = temporaryItemURL
+            print("\n")
+            print("INFO: FILE ATTRIBUTES FOR SANDBOXED PICKED DOCUMENT AT \(temporaryItemURL.path)")
+            printFileAttributes(path: temporaryItemURL.path)
             if securityScoped {
                 sourceItemURL.stopAccessingSecurityScopedResource()
             }
